86d0b0e4ceb5c2da5b10e88610a0b04b003fa3ea
alu0101160337 <alu0101160337@ull.edu.es>
2020-10-19 16:48:32 +0100

    First Commit

---
+++ Assets/Scripts/ConsoleTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ConsoleTest : MonoBehaviour
+{
+
+    private int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        List<GameObject> rootObjects = new List<GameObject>();
+        Scene scene = SceneManager.GetActiveScene();
+        scene.GetRootGameObjects(rootObjects);
+
+        // iterate root objects and do something
+        for (int i = 0; i < rootObjects.Count; ++i)
+        {
+            GameObject g = rootObjects[i];
+            UnityEngine.Component[] comps = g.GetComponents(typeof(PropertiesTest));
+            string msg = "Nombre : " + g.name;
+            if (comps.Length > 0 )
+            {
+                PropertiesTest t = (PropertiesTest)comps[0];
+                msg += " Id : " + t.Id;
+            }
+            msg += ". Valor contador = " + (++count); 
+            UnityEngine.Debug.Log(msg);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/PropertiesTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropertiesTest : MonoBehaviour
+{
+    private static int count = 0;
+    private int id;
+
+    public int Id { get => id; /*set => id = value;*/ }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = count + 1;
+        count++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
